9f2940c2a27460394ee73ffd758cdd1b29c0ef76
D1M30 <113018363+D1M30@users.noreply.github.com>
2022-12-06 19:06:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,100 @@
+"use strict"
+
+// let userAge = 33;
+// let userName = "Dima";
+
+// userAge = 44;
+// console.log(userAge + userName);
+
+
+// let userCount = 51;
+// let userCount2;
+
+// userCount2 = userCount;
+
+// console.log(userCount2);
+
+
+// let nameDog = `Snoopy`;
+// let nameCat;
+// nameDog = 45;
+// nameCat = nameDog;
+
+
+// console.log(nameCat);
+
+// ==============блок================
+
+// let userAge = 10;
+
+//    if (1 > 0) {
+//       let userAge = 20;
+//       console.log(userAge);
+//       if(10 > 5) {
+
+//       }
+
+//    }
+//    console.log(userAge);
+   
+
+// let userDog = null;
+
+// console.log(userDog);
+// console.log(typeof userDog);  
+
+
+// let count = 10 / 0;
+// console.log(count); 
+
+// let result = `dog` * 20;
+// console.log(result); 
+
+// let name = `Carl`;
+// let number = 55;
+
+// console.log(typeof name);
+// console.log(name);
+// console.log(typeof number);
+// console.log(number);
+
+
+// const myName = `Dima`;
+// const myAge = 30;
+// const myInfo =
+//  `Name: ${myName}
+//    Age: ${myAge}`;
+
+// console.log(myInfo)
+
+// const dogName = `jack`;
+// const dogAge = 4;
+
+// const dogInfo = `Name: ${dogName}, Age: ${dogAge}`;
+// console.log(dogInfo);
+
+// const someVar = 0;
+
+// console.log(typeof someVar);
+// console.log(someVar);
+
+// ---------------------------------------
+
+
+// ===========================================
+// ========= Домашка(переменные)----------
+
+// 1)
+let userEyesColor;
+console.log(`Eye color : ${userEyesColor}`);
+
+// 2)
+let user;
+let userName = `Вася`;
+user = userName;
+console.log(user);
+
+
+let userR = `33` / `88`;
+
+console.log(typeof userR)
